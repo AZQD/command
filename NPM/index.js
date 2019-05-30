a77d9c15f10f6fe0d46d4ea942f6ffe44964f89e
AZQD <604972395@qq.com>
2019-05-30 21:30:38 +0800

    index修改

--- NPM/index.js
+++ NPM/index.js
@@ -1,5 +1,8 @@
-export function consoleFun(str) {
-  console.log(str);
-}
-
-export const TextNode = 'This is my first NPM project';
+module.exports = {
+  consoleFun: function (str) {
+    console.log(str);
+  },
+  TextNode1: 'This is my first NPM project',
+  TextNode2: 'This is my first NPM project',
+  TextNode3: 'This is my first NPM project'
+};
